ac44639e378aef735664379c3bea47cc2d73ea36
Skyler Riggle <skyler32702@gmail.com>
2023-03-12 17:07:42 -0500

    Routing

--- src/Site.tsx
+++ src/Site.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const Site = () =>
 {
     return (
-        <></>
+        <>
+            Hello World
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<p>Hello World!</p>} />
+                    <Route path="/test" element={<p>Hello Test!</p>} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
