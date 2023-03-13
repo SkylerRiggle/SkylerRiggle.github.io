6257b9f3b6de8052bfe910302df6df35a5c0c702
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 18:16:14 -0500

    NavBar V1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,23 @@
+import Logo from "../images/logo.webp";
+
 const NavBar = () =>
 {
     return (
-        <></>
+        <div className="navbar y-auto px-4">
+            <a href="/portfolio" className="text-decoration-none">
+                <h3 className="fst-italic">
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        height="50rem"
+                        style={{
+                            filter: 'drop-shadow(0 0 5px green)'
+                        }}
+                    />
+                    &ensp;Skyler Riggle
+                </h3>
+            </a>
+        </div>
     );
 }
 
