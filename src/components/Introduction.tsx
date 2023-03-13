d4921dd8f634676d3a58fc679e68206c1dd1f951
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 07:00:42 -0500

    Intro Spacing

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,23 +3,6 @@ import Portrait from '../images/portrait.jpg';
 const Introduction = () =>
 {
     return (
-        // <div
-        //     className='position-relative w-100 h-100 my-5 mx-5'
-        //     style={{
-        //         minHeight: '80vh'
-        //     }}
-        // >
-        //     <img
-        //         src={Portrait}
-        //         alt="Portrait"
-        //         className='rounded-circle'
-        //         style={{
-        //             minWidth: '200px',
-        //             width: '30%',
-        //             filter: 'grayscale(100%)'
-        //         }}
-        //     />
-
         //     <div
         //         className='position-absolute top-0'
         //         style={{
@@ -36,51 +19,31 @@ const Introduction = () =>
         //             &emsp;&ensp;The University of Oklahoma
         //         </h2>
         //     </div>
-
-        //     <div
-        //         className='position-absolute'
-        //         style={{
-        //             left: '25%',
-        //             bottom: '5%'
-        //         }}
-        //     >
-        //         <h4><u>About Me</u></h4>
-        //         <blockquote className='blockquote fw-light'>
-        //             &emsp;Hello, my name is Skyler Riggle, and I'm a gamer, programmer, and an overall
-        //             technology enthusiast. As a result, I currently study Computer Science at the University 
-        //             of Oklahoma (Boomer Sooner!) and aspire to enter the video games industry to pursue my 
-        //             passions. Glad to meet you!
-        //         </blockquote>
-        //     </div>
-        // </div>
         
-        <>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-        </>
+        <div className='d-flex flex-wrap w-100 bg-success'>
+            {/** Portrait Section */}
+            <div className='bg-danger w-25 p-1 end-0' style={{minWidth:'200px', minHeight:'200px'}}>
+                <img
+                    src={Portrait}
+                    alt="Portrait"
+                    className='rounded-circle w-100'
+                    style={{
+                        filter: 'grayscale(100%)'
+                    }}
+                />
+            </div>
+            
+            {/** About Me Section */}
+            <div className='bg-primary w-75 p-1' style={{minWidth:'200px', minHeight:'200px'}}>
+                <h4><u>About Me</u></h4>
+                <blockquote className='blockquote fw-light'>
+                    &emsp;Hello, my name is Skyler Riggle, and I'm a gamer, programmer, and an overall
+                    technology enthusiast. As a result, I currently study Computer Science at the University 
+                    of Oklahoma (Boomer Sooner!) and aspire to enter the video games industry to pursue my 
+                    passions. Glad to meet you!
+                </blockquote>
+            </div>
+        </div>
     );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = (props: { children: any }) =>
     return (
         <div className="d-flex">
             <NavBar />
-            <div style={{marginLeft: '125px'}}>
+            <div className="w-100 my-4 me-4" style={{marginLeft: '110px'}}>
                 {props.children}
             </div>
         </div>
