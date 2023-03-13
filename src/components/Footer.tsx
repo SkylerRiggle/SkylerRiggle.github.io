f1c9f0d1b01c936f722f31beb6ea71634bf952ba
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 17:56:26 -0500

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () =>
+{
+    return (
+        <div className="footer text-muted">
+            Skyler Riggle / Portfolio Site / Last Updated March 2023
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,13 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
 const Layout = (props: { children: any }) =>
 {
     return (
         <>
+            <NavBar />
             {props.children}
+            <Footer />
         </>
     )
 }
