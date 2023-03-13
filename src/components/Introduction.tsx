a0c72953d98109bb7249446e432b8e7e7dbf9bfe
Skyler Riggle <skyler32702@gmail.com>
2023-03-12 20:36:42 -0500

    End Of Day Commit

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,58 @@
+import Portrait from '../images/portrait.jpg';
+
 const Introduction = () =>
 {
     return (
-        <>HELLO</>
+        <div
+            className='position-relative w-100 h-100 my-5 mx-5'
+            style={{
+                minHeight: '80vh'
+            }}
+        >
+            <img
+                src={Portrait}
+                alt="Portrait"
+                className='rounded-circle'
+                style={{
+                    minWidth: '200px',
+                    width: '30%',
+                    filter: 'grayscale(100%)'
+                }}
+            />
+
+            <div
+                className='position-absolute top-0'
+                style={{
+                    left: '24%'
+                }}
+            >
+                <h1 className='display-1 fw-bold text-stroke-lg'>
+                    Skyler Riggle
+                </h1>
+                <h2 className='display-6 fw-bold text-stroke-sm'>
+                    &emsp;Computer Science Student
+                </h2>
+                <h2 className='display-6 fw-bold text-stroke-sm'>
+                    &emsp;&ensp;The University of Oklahoma
+                </h2>
+            </div>
+
+            <div
+                className='position-absolute'
+                style={{
+                    left: '25%',
+                    bottom: '5%'
+                }}
+            >
+                <h4><u>About Me</u></h4>
+                <blockquote className='blockquote fw-light'>
+                    &emsp;Hello, my name is Skyler Riggle, and I'm a gamer, programmer, and an overall
+                    technology enthusiast. As a result, I currently study Computer Science at the University 
+                    of Oklahoma (Boomer Sooner!) and aspire to enter the video games industry to pursue my 
+                    passions. Glad to meet you!
+                </blockquote>
+            </div>
+        </div>
     );
 }
 
