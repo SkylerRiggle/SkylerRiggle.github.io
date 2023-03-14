4e8f5c9ed662fa904a073b86b309e05a3b63bf90
Skyler Riggle <skyler32702@gmail.com>
2023-03-14 15:12:03 -0500

    Career Highlight Abstraction

--- src/components/Home Page/Highlights.tsx
+++ src/components/Home Page/Highlights.tsx
@@ -1,7 +1,14 @@
 const Highlights = () =>
 {
     return (
-        <></>
+        <>
+            <h1 className="display-4 ms-3">Career Highlights</h1>
+            <hr className="mt-0 w-75 ms-3" />
+
+            <div>
+                
+            </div>
+        </>
     );
 }
 
