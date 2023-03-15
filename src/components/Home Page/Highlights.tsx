4c63b8a1388a65df1093ea02e583f26be509eea2
Skyler Riggle <skyler32702@gmail.com>
2023-03-14 20:03:19 -0500

    Empty Project Highlight Case

--- src/components/Home Page/Highlights.tsx
+++ src/components/Home Page/Highlights.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 const Highlights = () =>
 {
+    const [projectTiles, setProjectTiles] = useState<JSX.Element[]>([]);
+
+    const mapProjects = () =>
+    {
+
+    }
+    mapProjects();
+
     return (
         <>
             <h1 className="display-4 ms-3">Career Highlights</h1>
             <hr className="mt-0 w-75 ms-3" />
 
-            <div>
-                
+            <div
+                className="bg-accent mx-5 rounded d-flex flex-column align-items-center"
+                style={{
+                    boxShadow: 'inset 0 0 5px black'
+                }}
+            >
+                {
+                    projectTiles.length > 0 ?
+                        projectTiles
+                    :
+                        <code className="text-danger p-4">
+                            Sorry, Nothing To See Here...
+                        </code>
+                }
             </div>
         </>
     );
