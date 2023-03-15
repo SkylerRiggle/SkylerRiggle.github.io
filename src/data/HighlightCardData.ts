f904abe2dfc2ac75ab06f56848fb4ec0aaa3c431
Skyler Riggle <skyler32702@gmail.com>
2023-03-15 16:00:40 -0500

    Fixed Minor Warning ES5 Multiline

--- src/data/HighlightCardData.ts
+++ src/data/HighlightCardData.ts
@@ -14,10 +14,10 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "K20 Center's Game-Based Learning Team",
         imageSrc: GBL,
         href: "",
-        description: "Currently, I work as a Student Programmer at the \
-        K20 Center, where I've worked on many web-development projects \
-        such as a tool for updating content in an AR app and the game \
-        portal that hosts their games."
+        description: "Currently, I work as a Student Programmer at the" +
+        "K20 Center, where I've worked on many web-development projects" +
+        "such as a tool for updating content in an AR app and the game" +
+        "portal that hosts their games."
     },
 
     {
@@ -25,10 +25,10 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "OU Game Developer's Association",
         imageSrc: ENG,
         href: "",
-        description: "I am the active Vice President and Engineering \
-        Director for the University of Oklahoma's Game Developer's \
-        Association. In these positions I have organized many workshops, \
-        seminars, game jams, and general meetings."
+        description: "I am the active Vice President and Engineering" +
+        "Director for the University of Oklahoma's Game Developer's" +
+        "Association. In these positions I have organized many workshops," +
+        "seminars, game jams, and general meetings."
     },
 
     {
@@ -36,10 +36,10 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "OU GDA Showcase Event",
         imageSrc: CTC,
         href: "",
-        description: "A club I am involved with was asked to host a panel \
-        during the Anime Oklahoma convention. For this event, I developed \
-        Capture The Cow! A fun game where competing players battle to score \
-        points while flying clunky physics-based UFOs."
+        description: "A club I am involved with was asked to host a panel" +
+        "during the Anime Oklahoma convention. For this event, I developed" +
+        "Capture The Cow! A fun game where competing players battle to score" +
+        "points while flying clunky physics-based UFOs."
     },
 
     {
@@ -47,10 +47,10 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "Team Game Jam Project",
         imageSrc: Speculum,
         href: "",
-        description: "As a fun challenge, myself and a couple friends \
-        developed a small jam submission where I assumed the role of \
-        programmer. For the project, I developed a rudimentary combat \
-        system and various shader effects in the Unity game engine."
+        description: "As a fun challenge, myself and a couple friends" +
+        "developed a small jam submission where I assumed the role of" +
+        "programmer. For the project, I developed a rudimentary combat" +
+        "system and various shader effects in the Unity game engine."
     },
 
     {
@@ -58,10 +58,10 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "Personal Game Project",
         imageSrc: battle,
         href: "",
-        description: "Battle Ball was a fun personal project I developed \
-        in the span of about 3 weeks using the Unity game engine. The game \
-        was a 2-player in-browser game where players competed to either \
-        destroy the opponent's tank or score goals with the ball."
+        description: "Battle Ball was a fun personal project I developed" +
+        "in the span of about 3 weeks using the Unity game engine. The game" +
+        "was a 2-player in-browser game where players competed to either" +
+        "destroy the opponent's tank or score goals with the ball."
     },
 
     {
@@ -69,11 +69,11 @@ const highlightCardData: HighlightCardProps[] = [
         subTitle: "Personal University Game Project",
         imageSrc: NONO,
         href: "",
-        description: "For a University CS class on structures and \
-        abstractions I created a simple nonogram puzzle game from \
-        scratch using the JavaFX library. Related, I also created \
-        a small edge-detection app using the same library to generate \
-        puzzles from images."
+        description: "For a University CS class on structures and" +
+        "abstractions I created a simple nonogram puzzle game from" +
+        "scratch using the JavaFX library. Related, I also created" +
+        "a small edge-detection app using the same library to generate" +
+        "puzzles from images."
     }
 ];
 
