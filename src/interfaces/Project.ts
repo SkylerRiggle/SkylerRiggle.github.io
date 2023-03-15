41c35f0cbf28a60ccf559fc269d578b0580e791c
Skyler Riggle <skyler32702@gmail.com>
2023-03-14 19:00:23 -0500

    Project Data Abstraction

--- src/interfaces/Project.ts
+++ src/interfaces/Project.ts
@@ -1,4 +1,26 @@
-export default interface Project 
+export enum Skill
 {
-    
+    CPP,
+    Java,
+    JavaScript,
+    TypeScript
+}
+
+export enum LayoutItem
+{
+    TEXT,
+    IMAGE
+}
+
+export default interface ProjectData
+{
+    icon: string;
+    title: string;
+    time: string;
+    description: string;
+
+    skills?: Skill[];
+    pageLayout?: LayoutItem[];
+    paragraphs?: string[];
+    images?: { src: string, caption: string };
 }
