3c570139e434609ed69eebf877b469248e9cf751
Skyler Riggle <skyler32702@gmail.com>
2023-03-18 19:35:15 -0500

    Highlight Carousel

--- src/components/Global/Carousel.tsx
+++ src/components/Global/Carousel.tsx
@@ -1,3 +1,5 @@
+import Container from 'react-bootstrap/Carousel';
+
 export interface CarouselProps
 {
     items: any[];
@@ -6,9 +8,26 @@ export interface CarouselProps
 const Carousel = (props: CarouselProps) =>
 {
     return (
-        <div>
-            {props.items}
-        </div>
+        <Container
+            className="w-100 position-relative bg-accent mb-3"
+            style={{
+                height: '30rem',
+                boxShadow: 'inset 0 5px 5px 0 black, inset 0 -5px 5px 0 black'
+            }}
+            draggable
+        >
+            {
+                props.items.map((item: any, index: number) => {
+                    return (
+                        <Container.Item className='w-100' key={`Carousel-${index}`}>
+                            <div className='w-100 d-flex justify-content-center'>
+                                {item}
+                            </div>
+                        </Container.Item>
+                    )
+                })
+            }
+        </Container>
     );
 }
 
